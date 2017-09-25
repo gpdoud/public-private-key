89465e3f3ebc4ffbeb7b70b6656614b713d9e16f
Greg doud <gpdoud@gmail.com>
2017-09-24 23:02:54 -0400

    Completed first stage (simple version) of generation of public/private key.

---
+++ PublicPrivateKeyLib/PublicPrivateKey.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace PublicPrivateKeyLib {
+
+	public class PublicPrivateKey {
+
+		public long p { get; set; }
+		public long q { get; set; }
+		public long n { get { return p * q; } }
+		public long totientn { get { return (p - 1) * (q - 1); } }
+		public long mmie {
+			get {
+				var i = 0;
+				long x = -1;
+				while (x != 0) {
+					i++;
+					x = (e * i - 1) % totientn;
+				}
+				return i;
+			}
+		}
+		public long e { get; set; }
+		public long d { get; set; }
+
+		public long Encrypt(long m) {
+			return Power(m, e) % n;
+		}
+
+		public long Decrypt(long m) {
+			return Power(m, d) % n;
+		}
+
+		private long Power(long b, long e) {
+			//return BigInteger.Pow(b, e);
+			long ans = 1;
+			for (var idx = 0; idx < e; idx++) {
+				ans *= b;
+			}
+			return ans;
+		}
+
+		private long CalcModularMultiplicativeInverseOfE(long e, long totientn) {
+			long x = -1;
+			long i = 0;
+			while(x != 0) {
+				x = (e * i++ - 1) % totientn;
+			}
+			return x;
+		}
+
+		public bool isPrime(long number) {
+
+			if (number == 1) return false;
+			if (number == 2) return true;
+
+			for (int i = 2; i <= Math.Ceiling(Math.Sqrt(number)); ++i) {
+				if (number % i == 0) return false;
+			}
+
+			return true;
+
+		}
+	}
+}
---
+++ PublicPrivateKeyLibTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using PublicPrivateKeyLib;
+
+namespace PublicPrivateKeyLibTest {
+
+	class Program {
+
+		void RunTestPrime() {
+			PublicPrivateKey ppk = new PublicPrivateKey();
+			long prime = 1000000000000000000;
+			while(!ppk.isPrime(prime)) {
+				prime--;
+			}
+			Debug.WriteLine($"{prime} is prime");
+		}
+		void Run() {
+			PublicPrivateKey ppl = new PublicPrivateKey();
+			ppl.p = 3;
+			ppl.q = 11;
+			ppl.e = 17;
+			ppl.d = 13;
+
+			var enc = ppl.Encrypt(9);
+			Debug.WriteLine($"9 encrypted is {enc}");
+			var dec = ppl.Decrypt(15);
+			Debug.WriteLine($"15 decrypted is {dec}");
+		}
+		static void Main(string[] args) {
+			new Program().RunTestPrime();
+		}
+	}
+}
